40068664e729a30b91a4f9573edf2c76f6d10310
SrGiovanni <samuelefulghi@gmail.com>
2022-03-25 12:53:13 -0400

    reg commit 0.1

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection(
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
-      password: '',
+      password: DB_PASSWORD,
       database: 'election'
     },
     console.log('Connected to the election database.')
